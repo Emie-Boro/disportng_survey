93e4ca5b4969d33f71cc4bdf54149abc1bc28ab9
Emie Boro <emiedonmukumo@gmail.com>
2023-12-13 15:19:05 +0100

    Updated files

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.engine('.hbs', exphbs.engine({ extname:'.hbs' }))
+app.engine('.hbs', exphbs.engine({ extname:'.hbs', defaultLayout:'main' }))
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res)=>{
-    res.render('index', {title: 'Disport NG', layout:false})
+    res.render('index', {title: 'Disport NG', })
 })
 
 app.listen(8080, console.log('Server running...'))
