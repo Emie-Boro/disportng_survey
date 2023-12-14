932986435d5d56c24df72dcbe8b3afdbbd6ed22f
Emie Boro <emiedonmukumo@gmail.com>
2023-12-14 23:37:51 +0100

    Updated files

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const connectDB = require('./config/db');
 const path = require('path');
 const exphbs = require('express-handlebars') 
+const fs = require('fs');
+
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
@@ -13,38 +15,22 @@ app.engine('.hbs', exphbs.engine({ extname:'.hbs', defaultLayout:'main' }))
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-const envFile = path.resolve(__dirname, 'config', '.env');
-dotenv.config({ path: envFile });
-
-const MONGODB_URI = process.env.MONGO_URL;
-
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true, 
-})
-  .then(() => {
-    console.log("Connected to database")
-  })
-  .catch((err) => {
-    console.error("Error connecting to database:", err);
-  })
-
-  app.use(cors());
+dotenv.config({path: path.join(__dirname, '/config/.env')})
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const fs = require('fs');
+connectDB()
 
-const surveySchema = new mongoose.Schema({
+const surveySchema = mongoose.Schema({
   name: String,
   email: String,
   question1: String,
   question2: String,
   question3: String,
   question4: String,
-  question5: String,
+  // question5: String,
   question6: String,
   question7: String,
   question8: String,
@@ -52,22 +38,26 @@ const surveySchema = new mongoose.Schema({
 
 const Survey = mongoose.model('Survey', surveySchema)
 
+app.get('/', (req, res)=>{
+  res.render('index', {title: 'Disport NG', })
+})
+
 app.post('/api/new-survey', async (req, res) => {
   {
     try{
-      const { name, email, question1, question2, question3, question4, question5, question6, question7, question8,} = req.body
+      const { name, email, question1, question2, question3, question4, question6, question7, question8,} = req.body
 
-      const newSurvey = new Survey({ name, email, question1, question2, question3, question4, question5, question6, question7, question8, })
+      const newSurvey = await new Survey({ name, email, question1, question2, question3, question4, question6, question7, question8, })
       await newSurvey.save()
 
-      fs.appendFile('emails.txt', `${email},\n`, (err) => {
-        if (err) {
-          console.error('Error appending email to file:', err);
-          return res.status(500).json({ error: 'Error appending email to file' });
-        } else {
-          console.log('Email appended to file');
-        }
-      })
+      // fs.appendFile('emails.txt', `${email},\n`, (err) => {
+      //   if (err) {
+      //     console.error('Error appending email to file:', err);
+      //     return res.status(500).json({ error: 'Error appending email to file' });
+      //   } else {
+      //     console.log('Email appended to file');
+      //   }
+      // })
 
       res.status(201).json({ message: 'User data saved successfully'})
     } catch (error) {
@@ -76,8 +66,6 @@ app.post('/api/new-survey', async (req, res) => {
   }
 })
 
-app.get('/', (req, res)=>{
-    res.render('index', {title: 'Disport NG', })
-})
+
 
 app.listen(8080, console.log('Server running...'))
